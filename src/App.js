fb4986266b66390ca1db99b4ab1c509d891b06b5
Kamal Nanda <kamalnanda45@gmail.com>
2020-11-02 13:55:44 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './App.css';
+import SearchFilter from './SearchFilter';
+
+class App extends Component {
+  render() {
+    return <SearchFilter />
+  }
+}
+
+export default App;
